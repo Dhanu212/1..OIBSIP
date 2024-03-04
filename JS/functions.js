07f024783b3738e10c01ac6c80dd112ef64d6bb6
dandu1234 <155178099+dandu1234@users.noreply.github.com>
2024-03-04 21:15:16 +0530

    first commit

---
+++ JS/functions.js
@@ -0,0 +1,19 @@
+console.log(*'JS FUNCTIONS');
+function task(){
+    console.log('hello user');
+}
+//task();
+//function task(user,login){
+
+//}
+function(){
+    console.log("without name");
+}
+let res=function(){
+    console.log("without name");
+}
+res();
+//immediate
+(function(a,b){
+    alert(a+b);
+})(10,20)
